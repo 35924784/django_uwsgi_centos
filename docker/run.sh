302866511b1d5da92519c0019d40f50789b7aa1f
李志强 <35924784@qq.com>
2018-11-23 11:27:41 +0800

    解决centos启动后找不到证书问题。

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,6 @@
 /usr/local/nginx/sbin/nginx
 cd /project/MxOnline/ && uwsgi --ini /project/MxOnline/uwsgi.ini
 cd /project/MxOnline/ && python3 manage.py collectstatic --noinput
+ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N ""
+ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ed25519_key -N ""
 /usr/sbin/sshd -D
